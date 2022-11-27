db84b71795bc86a162a9905ebb2fc4517235719b
Sarah Kimak <sarahekimak@gmail.com>
2022-11-27 00:14:34 -0500

    updates sleep

--- src/main/java/com/example/ciscx82doodler/MainActivity.java
+++ src/main/java/com/example/ciscx82doodler/MainActivity.java
@@ -7,21 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 import com.google.android.material.slider.RangeSlider;
 
 public class MainActivity extends AppCompatActivity {
     private DrawView draw;
     private ImageButton imgColor;
-    private RangeSlider rangeSlider;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        draw = (DrawView) findViewById(R.id.draw_view);
-        rangeSlider = (RangeSlider) findViewById(R.id.brushSize);
+        //draw = (DrawView) findViewById(R.id.draw_view);
+        seekBar = (SeekBar) findViewById(R.id.brushSize);
         imgColor = (ImageButton) findViewById(R.id.chooseColor);
 
 /*
